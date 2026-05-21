ba5a83d9da1960db0b04b9de23dcd608f012e01f
sincere1229 <knakamura@donnoba.com>
2026-05-21 10:41:43 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Noto+Sans+JP:wght@300;400;500&family=Noto+Serif+JP:wght@300;400;500&display=swap" rel="stylesheet" />
       </head>
+      <head>
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Noto+Sans+JP:wght@300;400;500&family=Noto+Serif+JP:wght@300;400;500&display=swap" rel="stylesheet" />
+  <meta name="google-adsense-account" content="ca-pub-7983934375222521" />
+</head>
       <body>
         {children}
         <Script
